a7903a52d587469de97857993ac7aa8d29fe4fdc
Extazy-b <esikov506@gmail.com>
2025-03-14 13:26:50 +0300

    T5 final

--- T5.sql
+++ T5.sql
@@ -11,3 +11,12 @@ SELECT COUNT(DISTINCT Owner_ID) from Pet;
 SELECT Breed, COUNT(*) FROM Pet GROUP BY Breed;
 
 SELECT Breed, COUNT(*) FROM Pet GROUP BY Breed HAVING NOT COUNT(*) = 1;
+
+SELECT Nick, Age, Description from Pet WHERE Age BETWEEN 2 AND 5;
+/* выбираем питомцев активного возраста (не щенки и не пожилые) */
+
+SELECT First_Name, Phone, Address from Person WHERE Phone NOT LIKE '+7%';
+/* выбираем неформатные номера для их проверки */
+
+SELECT Owner_ID, Nick, Age, Breed from Pet WHERE Breed IN ('poodle', 'Siamese');
+/* выбираем питомцев конкретной породы */
